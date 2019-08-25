d7b44066621e52ad31b9fe208ace74fc532d3f59
Cristian Mejía <52016791+crmejiam@users.noreply.github.com>
2019-08-25 18:50:47 -0500

    codigo para crear y añadir la barra del menu de usuario y admin al jframe (comentado)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import view.MenuRegistrado.Informacionfunciones;
 import view.VistaPrincipal;
 
 public class Main {
+    /* Codigo para crear la barra de usuario y admin, hay que agregarla al JFrame cuando sea necesario
+    static JMenuBar barra = new JMenuBar();  //barra para las 3 pantallas de administrador y usuario, ponersela al JFrame
+    static JMenu archivo = new JMenu("Archivo");
+    static JMenu procesos = new JMenu("Procesos y Consultas");
+    static JMenu ayuda = new JMenu("Ayuda");
+    */
 	public static void main(String[] args) {
 		//Data.initModel();
+                /*
+                barra.add(archivo);
+                barra.add(procesos);
+                barra.add(ayuda);
+                frame.setJMenuBar(barra);
+                */
+                
 		VistaPrincipal s = new VistaPrincipal("Yo", new String[]{"Chupame", "La", "Verga"}, new Informacionfunciones());
 		s.run();
 		/*
