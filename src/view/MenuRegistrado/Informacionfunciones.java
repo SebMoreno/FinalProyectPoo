72a689c9c2b72a29a83366459517531f37985b35
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-08-25 14:18:06 -0500

    Informacionfunciones

--- src/view/MenuRegistrado/Informacionfunciones.java
+++ src/view/MenuRegistrado/Informacionfunciones.java
@@ -7,86 +7,74 @@ import java.awt.*;
 import java.util.Map;
 import javax.swing.*;
 
+public class Informacionfunciones extends JPanel {
+    
+	/**
+         * cosas que contiene el panel
+         */
+        private JButton go = new JButton("GO");
+        private JLabel Descripcion = new JLabel();
+        private Choice elegir_pelicula= new Choice();
+        private JLabel Titulo = new JLabel();
+        private JPanel panel_izq = new JPanel();
+        private JPanel panel_der = new JPanel();
+	
 
+        /**
+         * contructor de la clase , organiza los paneles 
+         */
+	public Informacionfunciones(){
+		
+                this.setLayout(new BorderLayout(7,15));
+		
 
+		/* Operaciones Adicionales */
+                panel_izq.setLayout(new BorderLayout(7,15));
+                panel_der.setLayout(new BorderLayout(7,15));
+                Titulo.setText("ELIJA SU PELICULA");
+		/* Fin Operaciones Adicionales */
 
-public class Informacionfunciones extends JPanel{
-    
-      /*
-    Declaracion de las variables
-    */
-    private JLabel titulo;
-    private JPanel panel1 ;
-    private JButton go;
-    private Choice c;
-    private JPanel panel2 ;
-    
-    /**
-     * Constructor de la vista
-     * contiene metodo que inicia todas las componentes
-     */
-   
-    public Informacionfunciones(){
-     inicializador();
-     iniciarComponentes();
-     add(panel1 , BorderLayout.EAST);
-     add(panel2, BorderLayout.WEST);
-     
-    }
-    private void inicializador(){
-        setBounds(10, 170, 375, 150);
-       setBackground(Color.WHITE);
-       setVisible(true);
-    
-}
-    
-    private void iniciarComponentes() {
-        /**
-         * TODO SOBRE EL SUBPANEL 1
-         */
-        
-        panel1=new JPanel();
-        panel1.setBackground(Color.yellow);
-        
-        /**
-         * Etiqueta para el titulo panel 1
-         */
-        
-        titulo=new JLabel("ELIJA UNA PELICULA");
-        
-        panel1.add(titulo , BorderLayout.CENTER);
-        
-        /**
-         * Boton choice para las peliculas
-         */
-        c=new Choice();
-        panel1.add(c,BorderLayout.CENTER);
-        panel1.add(go , BorderLayout.CENTER);
-        
-        /**
-         * Panel para las funciones 
-         */
-        panel2= new JPanel();
-        panel1.setBackground(Color.gray);
-        
-        
-        
-        }
-        /**
-         * Metodo para agregar peliculas en el choice
-         */
-    
-        /**public void choice (){
-            if (!gestorAplicacion.cine.Funcion.getFuncionesList().isEmpty()) {
-            for (Map.Entry<String, String[]> entry : gestorAplicacion.cine.Funcion.getFuncionesList().entrySet()) {
-                this.add((entry.getKey()));
+		/* Agregado de Componentes */
+                panel_izq.add(Titulo, BorderLayout.NORTH);
+                panel_izq.add(go, BorderLayout.SOUTH);
                 
-            }
-            
-        }
-        }**/
+                /**
+                 * EL BOTON CHOICE HAY QUE LLENARLO CON LAS PELICULAS A ESCOGER
+                 */
+                panel_izq.add(elegir_pelicula, BorderLayout.CENTER);
+                this.add(panel_izq, BorderLayout.CENTER);
+                
+                Descripcion.setText("INFORMACIÓN FUNCIONES");
+                panel_der.add(Descripcion, BorderLayout.NORTH);
+                panel_der.add(new PanelFunciones(), BorderLayout.CENTER);
+                this.add(panel_der, BorderLayout.EAST);
+		
+	}
+
+}
+class PanelFunciones extends JPanel{
     
-    
-    
-    
-    }
+       JPanel auxiliar = new JPanel();
+
+	public PanelFunciones(){
+		/**
+                 * organizacion de el panel
+                 */
+                this.setLayout(new BorderLayout(7,15));
+                auxiliar.setBackground(Color.WHITE);
+                this.add(auxiliar);
+                
+                /*
+                aqui deberia ir lo que contiene el panel
+                */
+                
+
+	}
+
+	/* Metodos auxiliares */
+	/* Fin Metodos auxiliares */
+
+}
+
+
+
---
+++ src/view/UsuarioAdministrador/CrearFuncion.java
@@ -0,0 +1,16 @@
+/**
+ *clase que contiene la vista de la opcion de menu informacion funciones
+* @author: Mariana Betancur
+ */
+package view.UsuarioAdministrador;
+import java.awt.*;
+import java.util.Map;
+import javax.swing.*;
+
+
+public class CrearFuncion extends JPanel {
+    
+    
+    
+    
+}
